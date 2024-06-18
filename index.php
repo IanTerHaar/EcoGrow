1624e09891955161b3f0886aa40fd412ac282047
Ian Ter Haar <ian.terhaar1004@gmail.com>
2024-06-18 18:35:38 +0200

    remove redundent commets in index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
-// Enable error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 ob_start();
-// Include database connection
 include 'includes/db.php';
 
-// Initialize an error message variable
 $error_message = '';
 
-// Check if the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -25,7 +21,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
         if (password_verify($password, $user['password'])) {
-            // Login successful
             session_start();
 
             $_SESSION['user_id'] = $user['userID'];
